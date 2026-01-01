d1f019ffb22ef14e83770245b803a6fa476b81a0
Mugiew Mac <iqooz9xmg@gmail.com>
2026-01-01 12:36:00 +0700

    first commit

--- app/Http/Controllers/DashboardPageController.php
+++ app/Http/Controllers/DashboardPageController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Player;
 use App\Models\Site;
-use App\Services\JayaPay\JayaPayClient;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardPageController extends Controller
@@ -27,28 +25,8 @@ class DashboardPageController extends Controller
         return view('site');
     }
 
-    public function bankManagement(JayaPayClient $client)
+    public function bankManagement()
     {
-        $user = Player::first();
-
-        $payload = [
-            'merchantCode' => config('qris.code'),
-            'orderType' => '0',
-            'method' => 'QRIS',
-            'orderNum' => now()->timestamp . rand(1000, 9999),
-            'payMoney' => 25000,
-            'productDetail' => str($user->username . ' Deposit')->limit(90, ''),
-            'notifyUrl' => config('qris.url'),
-            'dateTime' => now()->format('YmdHis'),
-            'expiryPeriod' => 5,
-            'name' => $user->username,
-            'email' => $user->email,
-            'phone' => $user->phone,
-        ];
-
-        $response = $client->createFiatOrder($payload);
-
-        dd($response);
         return view('bank');
     }
 
